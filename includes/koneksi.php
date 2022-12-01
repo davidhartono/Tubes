56eaf2dc8ac0caae51c3296110e447c483546604
David Hartono <108618053+davidhartono@users.noreply.github.com>
2022-12-01 16:01:13 +0700

    Add files via upload

---
+++ includes/koneksi.php
@@ -0,0 +1,25 @@
+<html>
+    <head><title>Koneksi</title></head>
+    <body>
+        
+        <?php
+
+        if(!isset($_SESSION)){
+            session_start();
+        }
+
+        $host = 'localhost';
+        $user = 'root';
+        $pass = '';
+        $database = 'david';
+
+        $koneksi = mysqli_connect($host,$user,$pass,$database);
+
+        if ($koneksi->connect_error){
+            die("Koneksi Gagal".$koneksi->connect_error);
+        } else 
+        
+
+        ?>
+    </body>
+</html>
