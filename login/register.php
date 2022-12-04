a6970c968fef44ce86b80796459a18b15ce3a178
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2022-12-04 10:37:02 +0700

    naroh echo kesalahan didalam field, naruh di login

--- login/register.php
+++ login/register.php
@@ -1,25 +1,126 @@
 <?php
-require_once '../includes/koneksi.php';
 
-$user = $_POST['username'];
-$pass = $_POST['password'];
-$nama = $_POST['nama'];
-$email = $_POST['email'];
-$alamat = $_POST['alamat'];
+require('../includes/koneksi.php');
 
-$sql = "INSERT INTO akun (username, password, nama, email, alamat) VALUES ('$user', '$pass', '$nama', '$email', '$alamat')";
+if (isset($_POST['btnsignup'])) {
+  $user = $_POST['username'];
+  $pass = $_POST['password']; // SHA1
+  $nama = $_POST['nama'];
+  $email = $_POST['email'];
 
-if($koneksi->query($sql)==TRUE){
+  $sql = "INSERT INTO akun (username,password,nama,email) VALUES ('$user','$pass','$nama','$email')";
+
+  if ($koneksi->query($sql) === TRUE) {
     echo "Registrasi Akun Anda Berhasil";
-} else {
-    echo "Terjadi kesalahan: ".$sql."<br/>".$koneksi->error;
+  } else {
+    echo "Terjadi kesalahan : " . $sql . "<br/>" . $koneksi->error;
+  }
+  $koneksi->close();
 }
-$koneksi->close();
-
 ?>
 
-<script>
-if ( window.history.replaceState ) {
-        window.history.replaceState( null, null, window.location.href );
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>COFTEA</title>
+  <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+</head>
+<link rel="stylesheet" href="./css/register.css">
+
+</head>
+
+<body>
+  <!-- partial:index.partial.html -->
+  <section class="forms-section">
+    <h1 class="section-title">COFTEA</h1>
+    <div class="forms">
+      <div class="form-wrapper is-active">
+        <button type="button" class="switcher switcher-signup">
+          Sign Up
+          <span class="underline"></span>
+        </button>
+        <form class="form form-login" method="POST">
+          <fieldset>
+            <legend>Please, enter your email and password for login.</legend>
+            <div class="input-block">
+              <label for="login-username">Username</label>
+              <input id="login-username" type="eusername" name="username" required>
+            </div>
+            <div class="input-block">
+              <label for="login-password">Password</label>
+              <input id="login-password" type="password" name="password" required>
+            </div>
+            <?php
+            if (isset($_POST['btnlogin'])) {
+              $user_login = $_POST['username'];
+              $user_pass = $_POST['password'];
+
+              $sql = "SELECT * FROM akun WHERE username = '$user_login' and password = '$user_pass'";
+              $query = mysqli_query($koneksi, $sql);
+
+              if (!$query) {
+                die("Query gagal" . mysqli_error($koneksi));
+              }
+              while ($row = mysqli_fetch_array($query)) {
+                $user = $row['username'];
+                $pass = $row['password'];
+                $nama = $row['nama'];
+                $email = $row['email'];
+              }
+
+              if ($user_login == $user && $user_pass == $pass) {
+                header("Location: ./admin/landingpage.php");
+                $_SESSION['username'] = $user;
+                $_SESSION['nama'] = $nama;
+                $_SESSION['email'] = $email;
+              } else {
+                echo "<br/><font color = red><b> User Tidak Ditemukan. </b></font>";
+              }
+            }
+            ?>
+          </fieldset>
+          <button type="submit" class="btn-login" name="btnlogin">Login</button>
+        </form>
+      </div>
+      <div class="form-wrapper">
+        <button type="button" class="switcher switcher-signup">
+          Sign Up
+          <span class="underline"></span>
+        </button>
+        <form class="form form-signup" method="POST">
+          <fieldset>
+            <legend>Please, enter your email, password and password confirmation for sign up.</legend>
+            <div class="input-block">
+              <label for="signup-nama">Nama</label>
+              <input id="signup-nama" type="text" name="nama" required>
+            </div>
+            <div class="input-block">
+              <label for="signup-username">Username</label>
+              <input id="signup-username" type="text" name="username" required>
+            </div>
+            <div class="input-block">
+              <label for="signup-email">E-mail</label>
+              <input id="signup-email" type="email" name="email" required>
+            </div>
+            <div class="input-block">
+              <label for="signup-password">Password</label>
+              <input id="signup-password" type="password" name="password" required>
+            </div>
+          </fieldset>
+          <button type="submit" class="btn-signup" name="btnsignup">Continue</button>
+        </form>
+      </div>
+    </div>
+  </section>
+  <!-- partial -->
+  <script src="./js/register.js"></script>
+  <script>
+    if (window.history.replaceState) {
+      window.history.replaceState(null, null, window.location.href);
     }
-</script>
+  </script>
+</body>
+
+</html>
