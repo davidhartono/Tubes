c1741a574c4b0360e874a277b12dc7caa499b78e
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2022-12-27 21:51:45 +0700

    ngubah warna sidebar halaman admin samain navbar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,5 +1,5 @@
 <div id="sidebar" class="active">
-    <div class="sidebar-wrapper active">
+    <div class="sidebar-wrapper active" style="background-color: #FFF3E4;">
         <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="logo">
@@ -17,26 +17,28 @@
         </div>
         <div class="sidebar-menu">
             <ul class="menu">
-                <li class="sidebar-title"><h5>Menu</h5></li>
+                <li class="sidebar-title">
+                    <h5>Menu</h5>
+                </li>
 
                 <li class="sidebar-item">
                     <a href="index.php" class='sidebar-link'>
                         <i class="bi bi-grid-fill"></i>
-                        <span>Dashboard</span>
+                        <span style="color: #483434;">Dashboard</span>
                     </a>
                 </li>
 
                 <li class="sidebar-item">
                     <a href="produk.php" class='sidebar-link'>
                         <i class="fa-solid fa-mug-hot"></i>
-                        <span>Produk</span>
+                        <span style="color: #483434;">Produk</span>
                     </a>
                 </li>
 
                 <li class="sidebar-item">
                     <a href="user.php" class='sidebar-link'>
-                    <i class="fa-solid fa-table"></i>
-                        <span>Data Customer</span>
+                        <i class="fa-solid fa-table"></i>
+                        <span style="color: #483434;">Data Customer</span>
                     </a>
                 </li>
             </ul>
