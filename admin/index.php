b566ee03c1d256f4c62e19310f78a2d6b5933450
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2022-12-30 21:33:20 +0700

    lupa hapus dark css

--- admin/index.php
+++ admin/index.php
@@ -15,7 +15,6 @@ if (empty($_SESSION['username'])) {
     <title>Dashboard</title>
 
     <link rel="stylesheet" href="assets/css/main/app.css">
-    <link rel="stylesheet" href="assets/css/main/app-dark.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="assets/css/shared/iconly.css">
 
