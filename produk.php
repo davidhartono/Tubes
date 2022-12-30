0a92cbbdba7a1e1e523d299e28d4c98fc845b93c
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2022-12-31 01:07:34 +0700

    perbaiki posisi script aja

--- produk.php
+++ produk.php
@@ -8,105 +8,103 @@ $query = mysqli_query($koneksi, "SELECT * FROM produk WHERE kategori = '$kategor
 
 include 'header.php';
 echo "<link rel='stylesheet' href='css/catalogue.css'>";
+echo "<script src='//cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
 ?>
 
 <!-- content section -->
 <div class="container">
    <?php
-   if($kategori == 'Cold Coffee'){?>
-   <h3 class="title"> Cold Coffees </h3>
+   if ($kategori == 'Cold Coffee') { ?>
+      <h3 class="title"> Cold Coffees </h3>
    <?php
    }
    ?>
    <?php
-   if($kategori == 'Hot Coffee'){?>
-   <h3 class="title"> Hot Coffees </h3>
+   if ($kategori == 'Hot Coffee') { ?>
+      <h3 class="title"> Hot Coffees </h3>
    <?php
    }
    ?>
    <?php
-   if($kategori == 'Cold Tea'){?>
-   <h3 class="title"> Cold Tea </h3>
+   if ($kategori == 'Cold Tea') { ?>
+      <h3 class="title"> Cold Tea </h3>
    <?php
    }
    ?>
    <?php
-   if($kategori == 'Hot Tea'){?>
-   <h3 class="title"> Hot Tea </h3>
+   if ($kategori == 'Hot Tea') { ?>
+      <h3 class="title"> Hot Tea </h3>
    <?php
    }
    ?>
-   
+
    <div class="products-container">
-        <?php
-        while ($data = mysqli_fetch_assoc($query)) {
-        ?>
+      <?php
+      while ($data = mysqli_fetch_assoc($query)) {
+      ?>
 
-        <div class="product">
+         <div class="product">
             <a href="">
-                <form action="" method="post">
-                    <div class="box">
-                        <img src="admin/upload/<?= $data['foto']; ?>" alt="">
-                        <h3><?= $data['nama']; ?></h3>
-                        <div class="price">Rp <?= $data['harga']; ?>/-</div>
-                        <input type="hidden" name="nama_produk" value="<?= $data['nama']; ?>">
-                        <input type="hidden" name="harga_produk" value="<?= $data['harga']; ?>">
-                        <select name="kategori" hidden>
-                            <option value="<?= $data['kategori'] ?>"></option>
-                        </select>
-                        <input type="hidden" name="foto_produk" value="<?= $data['foto']; ?>">
-                        <input type="submit" class="btn" value="Add To Cart" name="tambah_ke_cart">
-                    </div>
-                </form>
+               <form action="" method="post">
+                  <div class="box">
+                     <img src="admin/upload/<?= $data['foto']; ?>" alt="">
+                     <h3><?= $data['nama']; ?></h3>
+                     <div class="price">Rp <?= $data['harga']; ?>/-</div>
+                     <input type="hidden" name="nama_produk" value="<?= $data['nama']; ?>">
+                     <input type="hidden" name="harga_produk" value="<?= $data['harga']; ?>">
+                     <select name="kategori" hidden>
+                        <option value="<?= $data['kategori'] ?>"></option>
+                     </select>
+                     <input type="hidden" name="foto_produk" value="<?= $data['foto']; ?>">
+                     <input type="submit" class="btn" value="Add To Cart" name="tambah_ke_cart">
+                  </div>
+               </form>
             </a>
-        </div>
+         </div>
 
-        <?php
-        }
+      <?php
+      }
 
-        ?>
-        <?php
-    if (isset($_POST['tambah_ke_cart'])) {
-    ?>
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <?php
-        $nama_produk = $_POST['nama_produk'];
-        $harga_produk = $_POST['harga_produk'];
-        $kategori = $_POST['kategori'];
-        $foto_produk = $_POST['foto_produk'];
-        $jumlah_produk = 1;
+      ?>
+      <?php
+      if (isset($_POST['tambah_ke_cart'])) {
+      ?>
 
-        $select_cart = mysqli_query($koneksi, "SELECT * FROM cart WHERE nama = '$nama_produk'");
+         <?php
+         $nama_produk = $_POST['nama_produk'];
+         $harga_produk = $_POST['harga_produk'];
+         $kategori = $_POST['kategori'];
+         $foto_produk = $_POST['foto_produk'];
+         $jumlah_produk = 1;
 
-        if (mysqli_num_rows($select_cart) > 0) {
-        ?>
+         $select_cart = mysqli_query($koneksi, "SELECT * FROM cart WHERE nama = '$nama_produk'");
 
+         if (mysqli_num_rows($select_cart) > 0) {
+         ?>
             <script>
-                Swal.fire({
-                    icon: 'warning',
-                    text: 'Produk Sudah Ada di Cart'
-                })
+               Swal.fire({
+                  icon: 'warning',
+                  text: 'Produk Sudah Ada di Cart'
+               })
             </script>
-        <?php
-        } else {
+         <?php
+         } else {
             $insert_product = mysqli_query($koneksi, "INSERT INTO cart (nama, harga, kategori, foto, jumlah) VALUES('$nama_produk', '$harga_produk', '$kategori', '$foto_produk', '$jumlah_produk')");
-        ?>
+         ?>
             <script>
-                Swal.fire({
-                    icon: 'success',
-                    text: 'Produk Berhasil Ditambahkan ke Cart'
-                }).then(function() {
-                    window.location = "menu.php";
-                })
+               Swal.fire({
+                  icon: 'success',
+                  text: 'Produk Berhasil Ditambahkan ke Cart'
+               })
             </script>
-    <?php
-        }
-    }
+      <?php
+         }
+      }
 
-    ?>
-    </div>
+      ?>
+   </div>
 </div>
-      
+
 
 <!-- <div class="preview" >
 <div class="products-preview">
