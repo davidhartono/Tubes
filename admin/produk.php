2e4b1f6b09173140355ffb1b087a8e058a2987a0
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-01-02 20:35:02 +0700

    kasih excel pdf dll

--- admin/produk.php
+++ admin/produk.php
@@ -21,6 +21,7 @@ if (empty($_SESSION['username'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="assets/css/shared/iconly.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.2/css/buttons.bootstrap5.min.css">
 </head>
 
 <body>
@@ -51,7 +52,7 @@ if (empty($_SESSION['username'])) {
                                             Produk</a>
                                         <div class="mt-2">
 
-                                            <div class="table-responsive mt-3">
+                                            <div class="table-responsive mt-3" id="tabelwrapper">
                                                 <table class="table table-striped" id="tabelproduk">
                                                     <thead class="table-dark">
                                                         <tr>
@@ -68,7 +69,8 @@ if (empty($_SESSION['username'])) {
                                                         if ($jumlahProduk == 0) {
                                                         ?>
                                                             <tr>
-                                                                <td class="text-center" colspan="6">Data produk tidak tersedia</td>
+                                                                <td class="text-center" colspan="6">Data produk tidak
+                                                                    tersedia</td>
                                                             </tr>
                                                             <?php
                                                         } else {
@@ -78,13 +80,15 @@ if (empty($_SESSION['username'])) {
                                                             ?>
                                                                 <tr>
                                                                     <td><?= $jumlah ?></td>
-                                                                    <td><img src="./upload/<?= $data['foto'] ?>" width="100"></td>
+                                                                    <td><img src="./upload/<?= $data['foto'] ?>" width="100">
+                                                                    </td>
                                                                     <td><?= $data['nama'] ?></td>
                                                                     <td><?= number_format($data['harga']) ?></td>
                                                                     <td><?= $data['kategori'] ?></td>
                                                                     <td class="text-center">
                                                                         <a href="produk-detail.php?p=<?= $data['id']; ?>" class="btn btn-info"><i class="fas fa-search"></i>&nbsp;Detail</a> |
-                                                                        <a href="update-produk.php?p=<?= $data['id']; ?>" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i>&nbsp;Update</a> |
+                                                                        <a href="update-produk.php?p=<?= $data['id']; ?>" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i>&nbsp;Update</a>
+                                                                        |
                                                                         <a href="delete-produk.php?p=<?= $data['id']; ?>" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin menghapus')"><i class="fa-solid fa-trash"></i>&nbsp;Delete</a>
                                                                     </td>
                                                                 </tr>
@@ -123,9 +127,25 @@ if (empty($_SESSION['username'])) {
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+    <script src=" https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.bootstrap5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.colVis.min.js"></script>
     <script>
         $(document).ready(function() {
-            $('#tabelproduk').DataTable();
+            var table = $('#tabelproduk').DataTable({
+                buttons: ['copy', 'csv', 'excel', 'pdf', 'colvis'],
+                dom: "<'row'<'col-md-3'l><'col-md-6'B><'col-md-3'f>>" +
+                    "<'row'<'col-md-12'tr>>" +
+                    "<'row'<'col-md-5'i><'col-md-7'p>>"
+            });
+
+            table.buttons().container()
+                .appendTo('#tabelwrapper .col-md-6:eq(0)');
         });
     </script>
 </body>
