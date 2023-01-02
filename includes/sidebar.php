d33ded99b4cdab16ea6cd9cd59942635aafd0eee
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-01-02 19:08:07 +0700

    images ama media

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -48,6 +48,13 @@
                         <span style="color: #483434;">Order</span>
                     </a>
                 </li>
+
+                <li class="sidebar-item">
+                    <a href="akunadmin.php" class='sidebar-link'>
+                        <i class="fa-solid fa-user-pen"></i>
+                        <span style="color: #483434;">Edit Akun</span>
+                    </a>
+                </li>
             </ul>
         </div>
     </div>
