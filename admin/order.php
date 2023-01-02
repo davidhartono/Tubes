9dd1ef7137b91a26c3f87a55c13eaa709719cf3e
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2023-01-02 20:34:21 +0700

    buat excel pdf dlll, total harga

--- admin/order.php
+++ admin/order.php
@@ -13,12 +13,13 @@ if (empty($_SESSION['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Customer</title>
+    <title>Data Order</title>
 
     <link rel="stylesheet" href="assets/css/main/app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="assets/css/shared/iconly.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.2/css/buttons.bootstrap5.min.css">
 
 </head>
 
@@ -46,8 +47,8 @@ if (empty($_SESSION['username'])) {
                                         <h4>Tabel Orderan</h4>
                                     </div>
                                     <div class="card-body">
-                                        <div class="table-responsive mt-3">
-                                            <table class="table table-striped" id="tabelakun">
+                                        <div class="table-responsive mt-3" id="tabelwrapper">
+                                            <table class="table table-striped" id="tabelorder">
                                                 <thead class="table-dark">
                                                     <tr>
                                                         <th>No Order</th>
@@ -69,7 +70,7 @@ if (empty($_SESSION['username'])) {
                                                             <td><?= $data['username'] ?></td>
                                                             <td><?= $data['produk'] ?></td>
                                                             <td><?= $data['item'] ?></td>
-                                                            <td><?= $data['harga'] ?></td>
+                                                            <td><?= number_format($data['harga'] * $data['item']) ?></td>
                                                             <td class="text-center">
                                                                 <a href="selesai-order.php?no=<?= $data['no']; ?>" class="btn btn-success"><i class="fa-solid fa-check-square"></i></a> |
                                                                 <a href="delete-order.php?no=<?= $data['no']; ?>" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin menghapus')"><i class="fa-solid fa-trash"></i></a>
@@ -109,9 +110,25 @@ if (empty($_SESSION['username'])) {
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+    <script src=" https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.bootstrap5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.colVis.min.js"></script>
     <script>
         $(document).ready(function() {
-            $('#tabelakun').DataTable();
+            var table = $('#tabelorder').DataTable({
+                buttons: ['copy', 'csv', 'excel', 'pdf', 'colvis'],
+                dom: "<'row'<'col-md-3'l><'col-md-6'B><'col-md-3'f>>" +
+                    "<'row'<'col-md-12'tr>>" +
+                    "<'row'<'col-md-5'i><'col-md-7'p>>"
+            });
+
+            table.buttons().container()
+                .appendTo('#tabelwrapper .col-md-6:eq(0)');
         });
     </script>
 
